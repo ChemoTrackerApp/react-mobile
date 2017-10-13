a95d76d53071d76e73634e8e5e954c384d94cb41
Janice Patricia <janice_patricia@live.com>
2017-10-13 00:17:28 -0400

    fixes on navigation

--- ChemoTracker/components/Profile.js
+++ ChemoTracker/components/Profile.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { TabNavigator } from 'react-navigation';
-import Navigation from './Navigation.js';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import styles from '../styles/main.js';
 import color from '../styles/color.js';
