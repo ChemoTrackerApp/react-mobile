a586c95692bc19ba9aee191f0edfa97da2335d09
Janice Patricia <janice_patricia@live.com>
2017-11-04 14:18:04 -0400

    changed datepicker into datetimepicker

--- ChemoTracker/components/Calendar/CalendarEvent/event.js
+++ ChemoTracker/components/Calendar/CalendarEvent/event.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { TextInput, View, Button } from 'react-native';
+import { TextInput, Text, View, ScrollView, Button } from 'react-native';
 import Icon from 'react-native-vector-icons/Octicons';
 import DatePicker from 'react-native-datepicker';
 import styles from '../../../styles/main.js';
@@ -12,8 +12,10 @@ class CalendarEvent extends Component {
     super(props);
     console.log("props", this.props);
     this.state = {
-      date: props.navigation.state.params.date
+      date: props.navigation.state.params.date,
+      datetime: `${props.navigation.state.params.date} 20:00`
     }
+    this.buttonPressed = this.buttonPressed.bind(this);
   }
 
   static navigationOptions = {
@@ -21,37 +23,68 @@ class CalendarEvent extends Component {
     tabBarIcon: () => (<Icon size={24} name="calendar" color={color.navBarIcon} />)
   }
 
+  buttonPressed() {
+    console.log("buttonPressed");
+  }
+
   render() {
     console.log("props",this.props)
     return (
       <View style={styles.container}>
-        <TextInput
-          style={eventStyles.eventTitle}
-          placeholder="Title"
-        />
-        <DatePicker
-          style={{width: 200}}
-          date={this.state.date}
-          mode="date"
-          placeholder="select date"
-          format="YYYY-MM-DD"
-          minDate="2017-07-01"
-          maxDate="2017-12-31"
-          confirmBtnText="Confirm"
-          cancelBtnText="Cancel"
-          customStyles={{
-            dateIcon: {
-              position: 'absolute',
-              left: 0,
-              top: 4,
-              marginLeft: 0
-            },
-            dateInput: {
-              marginLeft: 36
-            }
-          }}
-          onDateChange={(date) => {this.setState({date: date})}}
-        />
+        <ScrollView>
+          <TextInput
+            style={eventStyles.eventTitle}
+            placeholder="Title"
+          />
+        <Text>From</Text>
+          <DatePicker
+            style={{width: 200}}
+            date={this.state.datetime}
+            mode="datetime"
+            format="YYYY-MM-DD HH:mm"
+            confirmBtnText="Confirm"
+            cancelBtnText="Cancel"
+            customStyles={{
+              dateIcon: {
+                position: 'absolute',
+                left: 0,
+                top: 4,
+                marginLeft: 0
+              },
+              dateInput: {
+                marginLeft: 36
+              }
+            }}
+            minuteInterval={10}
+            onDateChange={(datetime) => {this.setState({datetime1: datetime});}}
+          />
+        <Text>To</Text>
+          <DatePicker
+            style={{width: 200}}
+            date={this.state.datetime}
+            mode="datetime"
+            format="YYYY-MM-DD HH:mm"
+            confirmBtnText="Confirm"
+            cancelBtnText="Cancel"
+            customStyles={{
+              dateIcon: {
+                position: 'absolute',
+                left: 0,
+                top: 4,
+                marginLeft: 0
+              },
+              dateInput: {
+                marginLeft: 36
+              }
+            }}
+            minuteInterval={10}
+            onDateChange={(datetime) => {this.setState({datetime1: datetime});}}
+          />
+        <View style={{flexDirection: 'row', height: 80, padding: 20}}>
+          <Button title="Cancel" onPress={this.buttonPressed}/>
+          <Button title="Save" onPress={this.buttonPressed}/>
+        </View>
+        </ScrollView>
       </View>
     )
   }
