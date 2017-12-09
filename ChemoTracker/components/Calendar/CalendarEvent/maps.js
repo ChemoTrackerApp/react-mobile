947b36f7ab8c772efadba5a7398c7e747c953aef
jpatricia <janice_patricia@live.com>
2017-12-09 00:23:58 -0500

    added search bar into event map

--- ChemoTracker/components/Calendar/CalendarEvent/maps.js
+++ ChemoTracker/components/Calendar/CalendarEvent/maps.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import { TextInput, Text, View, ScrollView, Button, TouchableOpacity } from 'react-native';
+import {
+  TextInput,
+  Text,
+  View,
+  ScrollView,
+  Button,
+  TouchableOpacity,
+  StyleSheet
+} from 'react-native';
 import Icon from 'react-native-vector-icons/Octicons';
 import styles from '../../../styles/main.js';
 import color from '../../../styles/color.js';
 import { mapStyles } from '../../../styles/calendar.js';
 import MapView from 'react-native-maps';
 import Geocoder from 'react-native-geocoding';
+import SearchBar from 'react-native-material-design-searchbar';
+import { width, height, totalSize } from 'react-native-dimension';
 import _ from 'lodash';
-import moment from 'moment';
 
 class Maps extends Component {
 
@@ -61,24 +70,38 @@ class Maps extends Component {
     const { location, coordinate } = this.state;
     return (
       <View style={styles.container}>
-        <MapView
-          style={mapStyles.map}
-          provider={"google"}
-          onMapReady={this.mapRendered}
-          onPress={e => this.onPress(e.nativeEvent)}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        >
-          {_.isEmpty(coordinate) ? null :
-            <MapView.Marker
-              coordinate={coordinate}
-            />
-          }
-        </MapView>
+        <ScrollView>
+          <SearchBar
+            onSearchChange={() => console.log('On Search Change')}
+            height={height(8)}
+            onFocus={() => console.log('On Focus')}
+            onBlur={() => console.log('On Blur')}
+            placeholder={'Search...'}
+            autoCorrect={false}
+            returnKeyType={'search'}
+            inputStyle={StyleSheet.flatten(mapStyles.searchBar)}
+          />
+          <View style={mapStyles.mapContainer}>
+            <MapView
+              style={mapStyles.map}
+              provider={"google"}
+              onMapReady={this.mapRendered}
+              onPress={e => this.onPress(e.nativeEvent)}
+              initialRegion={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+              }}
+            >
+              {_.isEmpty(coordinate) ? null :
+                <MapView.Marker
+                  coordinate={coordinate}
+                />
+              }
+            </MapView>
+          </View>
+        </ScrollView>
       </View>
     )
   }
--- ChemoTracker/styles/calendar.js
+++ ChemoTracker/styles/calendar.js
@@ -106,11 +106,18 @@ export const eventStyles = StyleSheet.create({
 });
 
 export const mapStyles = StyleSheet.create({
+  mapContainer: {
+    height: height(92)
+  },
   map: {
     position: 'absolute',
     top: 0,
     left: 0,
     right: 0,
-    bottom: 0,
+    bottom: 0
   },
+  searchBar: {
+    width: width(100),
+    flex:1
+  }
 });
