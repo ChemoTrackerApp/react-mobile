73e7ab22f6399244f272913a248fb859d436e874
Jessica Hanta <jesshanta@gmail.com>
2018-01-14 19:53:57 -0500

    Setting status bar colour for profile to be light-content
    To Do - need to modify and include Status bar on every other view

--- ChemoTracker/components/Profile.js
+++ ChemoTracker/components/Profile.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { Text, View, Image } from 'react-native';
 import { TabNavigator } from 'react-navigation';
-import Navigation from './Navigation.js';
 import ProfileTop from './Profile/ProfileTop.js';
 import ProfileDetailsTable from './Profile/ProfileDetailsTable.js'
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import styles from '../styles/main.js';
 import color from '../styles/color.js';
+import { StatusBar } from 'react-native';
 
 class Profile extends Component {
 	static navigationOptions = {
@@ -15,7 +15,11 @@ class Profile extends Component {
  	}
 	render() {
     	return (
-      	<View style = { styles.profileMainContainer }>
+				<View style = { styles.profileMainContainer }>
+					<StatusBar
+						backgroundColor="transparent"
+						barStyle="light-content"
+						/>
 					<ProfileTop />
 					<ProfileDetailsTable />
       	</View>
