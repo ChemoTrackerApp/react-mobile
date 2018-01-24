4120c9fa25fcc0c14d19cd3947d6e49baf4e2b4e
Lakshini Sivasothy <lsivasot@edu.uwaterloo.ca>
2018-01-23 22:18:05 -0500

    Finished front-end for Intervention screen

--- ChemoTracker/styles/color.js
+++ ChemoTracker/styles/color.js
@@ -2,7 +2,11 @@ const color= {
   navBarIcon: '#EF7A5A',
   navBarBackground: '#FCFCFC',
   navBarLabel: '#4F4F4F',
-  navBarRipple: '#C7C7CD'
+  navBarRipple: '#C7C7CD',
+
+  trackBackground: '#FFF',
+  trackContent: '#4E4E4E',
+  trackTitle: '#EF7A5A',
 };
 
 export default color;
