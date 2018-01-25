e86ff249f49976ff26b697c4ee6cbf46e8892770
Oluwaseun Makinde <oluwaseunmakinde@Oluwaseuns-MacBook-Pro-2.local>
2018-01-25 02:14:55 -0500

    fixed compilation errors and got tab bar showing on the top yay

--- ChemoTracker/components/TrackTab.js
+++ ChemoTracker/components/TrackTab.js
@@ -1,47 +1,29 @@
 import React, { Component } from 'react';
 import { Button } from 'react-native';
-import { StackNavigator } from 'react-navigation';
+import { TabView, TabNavigator, StackNavigator } from 'react-navigation';
 import Icon from 'react-native-vector-icons/Octicons';
+import Home from './Home.js';
 import Track from './SymptomTracking/Track.js';
 import Intervention from './SymptomTracking/Intervention.js';
 import TrackHeader from './SymptomTracking/TrackHeader.js';
 import { headerStyles } from '../styles/SymptomTracking/track.js';
 
 
-const navOptionsTrack = ({navigation}) => (
-  {
-    headerStyle: headerStyles.headerBackground,
-    headerTitleStyle: headerStyles.headerTitleStyle,
-    headerBackTitleStyle: headerStyles.headerBackTitleStyle,
-    headerLeft: <TrackHeader
-      state={'track'}
-      navigation={navigation.state}
-      navigate={navigation.navigate}/>
-  }
-)
+const TrackTab = TabNavigator({
+  Nausea: { screen: Home, path: '' },
+  Vomiting: { screen: Home, path: '' },
+  Fatigue: { screen: Home, path: '' },
+  Diarrhea: { screen: Home, path: '' },
+  Constipation: { screen: Home, path: '' },
+  Mucositis: { screen: Home, path: '' },
+  HandFootSyndrome: { screen: Home, path: '' },
+  Rash: { screen: Home, path: '' },
+  NailChanges: { screen: Home, path: '' }
+}, {
+  tabBarPosition: 'top',
+  animationEnabled: true,
+  tabBarOptions: {
 
-const navOptionsIntervention = ({navigation}) => (
-  {
-    headerStyle: headerStyles.headerBackground,
-    headerTitleStyle: headerStyles.headerTitleStyle,
-    headerBackTitleStyle: headerStyles.headerBackTitleStyle,
-    headerLeft: <TrackHeader
-      state={'intervention'}
-      navigation={navigation.state}
-      navigate={navigation.navigate}/>
-  }
-)
-
-const TrackTab = StackNavigator({
-  Track: {
-    screen: Track,
-    path: 'track',
-    navigationOptions: navOptionsTrack
-  },
-  Intervention: {
-    screen: Intervention,
-    path: 'intervention',
-    navigationOptions: navOptionsIntervention
   }
 })
 
