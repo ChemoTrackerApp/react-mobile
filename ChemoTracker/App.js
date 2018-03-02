ec8179dad10f22fa86dd6273b71a3a146496bfe5
Oluwaseun Makinde <oluwaseunmakinde@Oluwaseuns-MacBook-Pro-2.local>
2018-03-02 01:32:21 -0500

    refactored. login shows up first. added stack nav for login and signup

--- ChemoTracker/App.js
+++ ChemoTracker/App.js
@@ -1,36 +1,7 @@
 import React from 'react';
 import { Platform } from 'react-native';
-import { NavigationComponent } from 'react-native-material-bottom-navigation';
-import { TabNavigator } from 'react-navigation';
-import Home from './components/Home.js';
-import Profile from './components/Profile.js';
-import Search from './components/Search.js';
-import Calendar from './components/Calendar.js';
-import TrackTab from './components/TrackTab.js';
-import color from './styles/color.js';
 
-const App = TabNavigator({
-  Home: { screen: Home, path: '' },
-  Track: { screen: TrackTab, path: 'track' },
-  Search: { screen: Search, path: 'search' },
-  Calendar: { screen: Calendar, path: 'calendar' },
-  Profile: { screen: Profile, path: 'profile' }
-}, {
-  tabBarComponent: NavigationComponent,
-  tabBarPosition: 'bottom',
-  tabBarOptions: {
-    bottomNavigationOptions: {
-      labelColor: color.navBarLabel,
-      rippleColor: color.navBarRipple,
-      tabs: {
-        Home: { barBackgroundColor: color.navBarBackground },
-        Profile: { barBackgroundColor: color.navBarBackground },
-        Track: { barBackgroundColor: color.navBarBackground },
-        Search: { barBackgroundColor: color.navBarBackground },
-        Calendar: { barBackgroundColor: color.navBarBackground }
-      }
-    }
-  }
-});
+import loginStackNav from './components/Login/Nav.js';
 
-export default App;
+
+export default loginStackNav;
