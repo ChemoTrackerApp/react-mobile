ad418b56d6adbd43602f68e9020ea0d83f72cbf5
Oluwaseun Makinde <oluwaseunmakinde@Oluwaseuns-MacBook-Pro-2.local>
2018-03-06 15:16:42 -0500

    finally fixed bug. done preliminary design

--- ChemoTracker/components/Login/Login.js
+++ ChemoTracker/components/Login/Login.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, Button, View, Alert, Image } from 'react-native';
+import { StyleSheet, Text, TextInput, Button, View, Alert, Image, TouchableOpacity } from 'react-native';
+import { LinearGradient, Font } from 'expo';
+
 
 import styles from '../../styles/login_screen.js';
 
 export default class Login extends React.Component {
+
+  componentDidMount() {
+   Font.loadAsync({
+     'open-sans-bold': require('../../assets/fonts/OpenSans-Bold.ttf'),
+  });
+}
+
+  onPress = () => {
+    Alert.alert("login pressed");
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -19,15 +32,34 @@ export default class Login extends React.Component {
          style={styles.textFieldContainer}
          placeholder="password"
        />
-       <View style={styles.buttonContainer}>
-         <Button
-          onPress={()=> {}}
-          title="Login"
-          color="#fff"
-          accessibilityLabel="Tap on Me"
-         />
-       </View>
-       <Text>New? <Text style={styles.signUpLink}>SIGN UP</Text></Text>
+       <TouchableOpacity
+        style={{width: '70.55%',  height: 29}}
+        onPress={this.loginClicked}>
+          <LinearGradient
+            colors={['#59D0C2','#066368']}
+            start={[0, 1]}
+            end={[1, 0]}
+            style={{ padding: 15, alignItems: 'center', borderRadius: 5 , marginTop: '10%'}}>
+            <Text
+              style={{
+                backgroundColor: 'transparent',
+                fontSize: 19,
+                color: '#fff',
+                fontWeight: 'bold',
+              }}>
+              Sign In
+            </Text>
+          </LinearGradient>
+        </TouchableOpacity>
+
+        <View style={{marginTop:'20%'}}>
+          <Text style={{fontSize:19}}>
+            <Text>New?</Text>
+            <Text style={styles.text} onPress={this.onPress}> Sign Up</Text>
+          </Text>
+        </View>
+
+
       </View>
     );
   }
