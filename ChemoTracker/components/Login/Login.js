28f10b5d68d332bb4320175e6f2d3885efd98ff9
Seun Makinde <seunmakinde@ymail.com>
2018-03-13 00:12:17 -0400

    Update Login.js

--- ChemoTracker/components/Login/Login.js
+++ ChemoTracker/components/Login/Login.js
@@ -11,7 +11,7 @@ export default class Login extends React.Component {
     fontLoaded: false,
     errorLabel: '',
     loginUsername: '',
-		loginPassword: '',
+    loginPassword: '',
     auth_token: ''
   };
   async componentDidMount() {
