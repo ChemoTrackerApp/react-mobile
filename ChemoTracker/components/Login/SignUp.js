6cda25c57cfb4564ff1948c11f3c8a540b3eddb6
Oluwaseun Makinde <oluwaseunmakinde@Oluwaseuns-MacBook-Pro-2.local>
2018-03-13 13:14:07 -0400

    connect sign up backend, fix text input events,etc

--- ChemoTracker/components/Login/SignUp.js
+++ ChemoTracker/components/Login/SignUp.js
@@ -3,8 +3,55 @@ import { StyleSheet, Text, TextInput, Button, View, Alert, Image, TouchableOpaci
 import { LinearGradient, Font } from 'expo';
 
 import styles from '../../styles/login_screen.js';
+import { register } from '../../services/login.js';
 
 export default class SignUp extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      fontLoaded: false,
+      errorLabel: '',
+      signUpEmail: '',
+      signUpPassword1: '',
+      signUpPassword2: '',
+      auth_token: ''
+    }
+  }
+  async onSubmit() {
+    this.setState({ errorLabel: '' });
+    if (this.state.signUpEmail=== '') {
+      this.setState({ errorLabel: 'Email is required' });
+      return;
+    }
+    if (this.state.signUpPassword1 === '') {
+      this.setState({ errorLabel: 'Password is required' });
+      return;
+    }
+    if (this.state.signUpPassword2 === '') {
+      this.setState({ errorLabel: 'Confirm Password is required' });
+      return;
+    }
+    register(this.state.signUpEmail, this.state.signUpPassword1,this.state.signUpPassword2)
+    .then((responseJson) => {
+      console.log("signup-responseJson", responseJson);
+      if(responseJson.key) {
+        this.setState({
+          auth_token: responseJson.key
+        });
+          this.props.navigation.navigate("Menu", { token: this.state.auth_token });
+      } else {
+        if(responseJson.email) {
+          console.log("email error: ", responseJson.email[0]);
+        } else if(responseJson.password) {
+          console.log("password error: ", responseJson.password[0]);
+        }
+      }
+      })
+    .catch((error) =>{
+      console.error(error);
+      this.setState({errorLabel: error});
+    });
+  }
 
   registerClicked = () => {
 
@@ -21,22 +68,37 @@ export default class SignUp extends React.Component {
           style={styles.logo}
         />
         <TextInput
+          keyboardType="email-address"
           underlineColorAndroid="transparent"
+          onChangeText={(text)=>{this.setState({signUpEmail:text})}}
+          onSubmitEditing={(event) => {
+            this.refs.PasswordField1.focus();
+          }}
           style={styles.textFieldContainer}
           placeholder="email"
         />
         <TextInput
+          ref ="PasswordField1"
           secureTextEntry={true}
           underlineColorAndroid="transparent"
+          onChangeText={(text)=>{this.setState({signUpPassword1:text})}}
           style={styles.textFieldContainer}
+          onSubmitEditing={(event) => {
+            this.refs.PasswordField2.focus();
+            this.onSubmit();
+          }}
           placeholder="password"
         />
         <TextInput
+          ref ="PasswordField2"
           secureTextEntry={true}
           underlineColorAndroid="transparent"
+          onChangeText={(text)=>{this.setState({signUpPassword2:text})}}
           style={styles.textFieldContainer}
+          onSubmitEditing={() => {  this.onSubmit();}}
           placeholder="confirm password"
         />
+        <Text style={styles.errorLabel} >{this.state.errorLabel}</Text>
         <TouchableOpacity
           style={styles.loginButton}
           onPress={this.registerClicked}>
@@ -50,7 +112,7 @@ export default class SignUp extends React.Component {
         </TouchableOpacity>
         <View>
           <Text style={styles.bottomSectionText}>
-            <Text>Already Registered?</Text>
+            <Text>Have an account?</Text>
             <Text style={styles.text} onPress={this.signInClicked}> Sign In</Text>
           </Text>
         </View>
--- ChemoTracker/services/login.js
+++ ChemoTracker/services/login.js
@@ -13,3 +13,17 @@ export const login = (emailInput, passwordInput) => {
     return res.json()
   })
 }
+
+export const register = (emailInput, passwordInput1, passwordInput2) => {
+  return fetch(`${api}/rest-auth/registration/`, {
+    method: 'post',
+    headers: header,
+    body: JSON.stringify({
+      email: emailInput,
+      password1: passwordInput1,
+      password2: passwordInput2
+    })
+  }).then(res => {
+    return res.json()
+  })
+}
