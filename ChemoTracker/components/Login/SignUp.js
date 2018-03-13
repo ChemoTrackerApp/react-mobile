3add38fbe95f86df21141a2fdcf3311dc11936b6
Lakshini Sivasothy <lsivasot@edu.uwaterloo.ca>
2018-03-12 22:30:21 -0400

    Fixed styling issues for the Login and Signup screens

--- ChemoTracker/components/Login/SignUp.js
+++ ChemoTracker/components/Login/SignUp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, Button, View, Alert, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TextInput, Button, View, Alert, Image, TouchableOpacity, StatusBar } from 'react-native';
 import { LinearGradient, Font } from 'expo';
 
 import styles from '../../styles/login_screen.js';
@@ -10,48 +10,46 @@ export default class SignUp extends React.Component {
 
   }
   signInClicked = () => {
-    this.props.navigation.navigate("SignIn");
+    this.props.navigation.navigate("Login");
   }
   render() {
     return (
       <View style={styles.container}>
+        <StatusBar hidden={true}/>
         <Image
           source={require('../../assets/img/chemotracker.png')}
           style={styles.logo}
         />
         <TextInput
+          underlineColorAndroid="transparent"
           style={styles.textFieldContainer}
           placeholder="email"
         />
-       <TextInput
-         style={styles.textFieldContainer}
-         placeholder="password"
-       />
-       <TextInput
-         style={styles.textFieldContainer}
-         placeholder="confirm password"
-       />
-       <TouchableOpacity
-        style={{width: '70.55%',  height: 29}}
-        onPress={this.registerClicked}>
+        <TextInput
+          secureTextEntry={true}
+          underlineColorAndroid="transparent"
+          style={styles.textFieldContainer}
+          placeholder="password"
+        />
+        <TextInput
+          secureTextEntry={true}
+          underlineColorAndroid="transparent"
+          style={styles.textFieldContainer}
+          placeholder="confirm password"
+        />
+        <TouchableOpacity
+          style={styles.loginButton}
+          onPress={this.registerClicked}>
           <LinearGradient
-            colors={['#59D0C2','#066368']}
+            colors={['#59D0C2', '#066368']}
             start={[0, 1]}
             end={[1, 0]}
-            style={{ padding: 15, alignItems: 'center', borderRadius: 5 , marginTop: '10%'}}>
-            <Text
-              style={{
-                backgroundColor: 'transparent',
-                fontSize: 19,
-                color: '#fff',
-                fontWeight: 'bold',
-              }}>
-              Sign Up
-            </Text>
+            style={styles.loginGradient}>
+            <Text style={styles.loginText}> Sign Up </Text>
           </LinearGradient>
         </TouchableOpacity>
-        <View style={{marginTop:'20%'}}>
-          <Text style={{fontSize:18}}>
+        <View>
+          <Text style={styles.bottomSectionText}>
             <Text>Already Registered?</Text>
             <Text style={styles.text} onPress={this.signInClicked}> Sign In</Text>
           </Text>
