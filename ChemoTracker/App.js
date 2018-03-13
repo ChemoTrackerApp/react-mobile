6a8d521e7f3b883d4263cb5290b736c03368e900
Jessica Hanta <jesshanta@gmail.com>
2018-03-13 11:49:18 -0400

    temp fixes and s3 connection

--- ChemoTracker/App.js
+++ ChemoTracker/App.js
@@ -1,6 +1,34 @@
 import React from 'react';
 import { Platform } from 'react-native';
-import loginStackNav from './components/Login/Nav.js';
+import { NavigationComponent } from 'react-native-material-bottom-navigation';
+import { TabNavigator } from 'react-navigation';
+import HomeStackNav from './components/HomeStackNav.js';
+import ProfileTab from './components/ProfileTab.js';
+import PatientEducationTab from './components/PatientEducationTab.js';
+import CalendarTab from './components/CalendarTab.js';
+import color from './styles/color.js';
 
+const Menu = TabNavigator({
+  Home: { screen: HomeStackNav, path: '' },
+  Search: { screen: PatientEducationTab, path: 'search' },
+  Calendar: { screen: CalendarTab, path: 'calendar' },
+  Profile: { screen: ProfileTab, path: 'profile' }
+}, {
+  initialRouteName: 'Home',
+  tabBarComponent: NavigationComponent,
+  tabBarPosition: 'bottom',
+  tabBarOptions: {
+    bottomNavigationOptions: {
+      labelColor: color.navBarLabel,
+      rippleColor: color.navBarRipple,
+      tabs: {
+        Home: { barBackgroundColor: color.navBarBackground },
+        Profile: { barBackgroundColor: color.navBarBackground },
+        Search: { barBackgroundColor: color.navBarBackground },
+        Calendar: { barBackgroundColor: color.navBarBackground }
+      }
+    }
+  }
+});
 
-export default loginStackNav;
+export default Menu;
